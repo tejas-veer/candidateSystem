b7b6faa6f475228e97c30c321d86b9fa17b73a5d
Pcode13 <veer.pragati99@gmail.com>
2022-08-28 00:00:34 +0530

    front page

--- candidateApp/urls.py
+++ candidateApp/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path,include
 # from .views import candidate_details, candidate_list
 # from .views import CandidateList,CandidateDetails
-from .views import CandidateModelViewSet,UserModelViewSet
+from .views import CandidateModelViewSet,UserModelViewSet,index
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
 from django.conf import settings
@@ -13,6 +13,7 @@ router.register('candi' , CandidateModelViewSet , basename='candi')
 router.register('user' , UserModelViewSet , basename='user')
 
 urlpatterns = [
+    path("", index , name="index"),
     path('api/', include(router.urls) , name='candi' ),
     path('api/', include(router.urls) , name='candi' ),
     path('api/auth/', obtain_auth_token  , name='auth_token' ),

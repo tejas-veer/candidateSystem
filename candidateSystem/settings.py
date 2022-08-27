2c0006c5a8aaeed2ca7730899ef81bfb56b1e969
Pcode13 <veer.pragati99@gmail.com>
2022-08-27 22:05:41 +0530

    allowed_host

--- candidateSystem/settings.py
+++ candidateSystem/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!zuc8sch5_9iiy_)nrk31c@%#+oa!hue%za5o@*94uspvk(i@g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['candidate-system.herokuapp.com','127.0.0.1']
 
 
 # Application definition

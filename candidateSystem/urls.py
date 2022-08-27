b3c62aa113e15fd6feb9c0a63cb82241d52ffe21
Pcode13 <veer.pragati99@gmail.com>
2022-08-27 22:08:55 +0530

    static root

--- candidateSystem/urls.py
+++ candidateSystem/urls.py
@@ -5,4 +5,4 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('candidateApp.urls'))
-]
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
